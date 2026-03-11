e59d2999aab7717cc8fa4ed4f3675b6327a9c655
hetexahir02 <hetexahir02@gmail.com>
2026-03-11 18:03:26 +0530

    change data export in media/urls.py
    add export press

--- django_project/media/urls.py
+++ django_project/media/urls.py
@@ -9,8 +9,5 @@ urlpatterns = [
     path("broadcast/", views.broadcast_view, name="broadcast"),
     path("highlights/", views.highlights_view, name="highlights"),
     path("press/", views.press_view, name="press"),
-    # data exports
-    path("export/press/", views.export_press_csv, name="export_press"),
-    path("export/broadcasts/", views.export_broadcast_csv, name="export_broadcasts"),
-    path("export/highlights/", views.export_highlights_csv, name="export_highlights"),
+    path("press/export/", views.export_press_csv, name="export_press"),
 ]
